3d54cca9b097653ae83fa75a0310610ef339b827
MilanR <milanche86ns@gmail.com>
2024-10-15 19:48:57 +0200

    prva jedinica gotova komentari i promenljive

--- 0-komentari-promenljive.py
+++ 0-komentari-promenljive.py
@@ -1,1 +1,31 @@
-print('Krecemo sa komentarima i promenljivama')
+# print('Krecemo sa komentarima i promenljivama')
+
+
+##############################
+##  Varijable/Promenljive   ##
+##############################
+naslov = 'Promeljive'                     
+print(naslov)
+ime = 'Freeda'
+godine = 4
+print("moj psic se zove "  + ime + " i ima " + str(godine) + ' godine')
+broj1 = 100
+broj2 = "200"
+print (broj1 +int(broj2))
+
+x = 100
+x = 'Sloba'
+print(x)
+
+a, b, c = 30, 390, 3009
+print (c)
+
+x = y = z = 200
+print(x, y, z)
+
+ime, prezime, email = 'Sloba', 'Miric', 'slobamiric@gmail.com'
+print( ime, prezime, email)
+
+auto = 'Mercedes' 
+AUTO = 'BMW'
+print(AUTO)
